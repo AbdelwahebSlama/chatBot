fd79ca6719d43c7be778faacdceac442a400f983
Slama Abdelwaheb <58324513+AbdelwahebSlama@users.noreply.github.com>
2020-07-14 13:17:32 +0200

    ** mettre en place le template de chat **** update1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,23 @@ import {AppComponent} from './app.component';
 import {MDBBootstrapModule} from 'angular-bootstrap-md';
 import {FormsModule} from '@angular/forms';
 import {ChatbotComponent} from './component/chatbot/chatbot.component';
+import {AttenteComponent} from './component/type/attente/attente.component';
+import {ImportFichierComponent} from './component/type/import-fichier/import-fichier.component';
+import {DedoublonnageComponent} from './component/type/dedoublonnage/dedoublonnage.component';
+import {LectureCrmComponent} from './component/type/lecture-crm/lecture-crm.component';
+import {ReceptionNFComponent} from './component/type/reception-nf/reception-nf.component';
+import {CadreProblemComponent} from './component/type/cadre-problem/cadre-problem.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ChatbotComponent,
+    AttenteComponent,
+    ImportFichierComponent,
+    DedoublonnageComponent,
+    LectureCrmComponent,
+    ReceptionNFComponent,
+    CadreProblemComponent,
   ],
   imports: [
     BrowserModule,
