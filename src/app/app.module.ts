f44a7ec393bfd8ff4b3bf4bc62ce8f1f29036df2
Slama Abdelwaheb <58324513+abdelwahebslama@users.noreply.github.com>
2020-07-14 11:32:32 +0200

    ** mettre en place le template de chat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+
+import {MDBBootstrapModule} from 'angular-bootstrap-md';
+import {FormsModule} from '@angular/forms';
+import {ChatbotComponent} from './component/chatbot/chatbot.component';
+import {ChatbotProblemComponent} from './component/chatbot-problem/chatbot-problem.component';
+import {CrmComponent} from './component/items/crm/crm.component';
+import {VoixComponent} from './component/items/voix/voix.component';
+import {BillingComponent} from './component/items/billing/billing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatbotComponent,
+    ChatbotProblemComponent,
+    CrmComponent,
+    VoixComponent,
+    BillingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    MDBBootstrapModule.forRoot(),
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
