ab812c55e36060c83cd790ccfa2566fc529ab1ee
Slama Abdelwaheb <58324513+AbdelwahebSlama@users.noreply.github.com>
2020-09-01 11:41:49 +0200

    *** laison avec base de données

---
+++ src/app/services/bd/add-respons.service.ts
@@ -0,0 +1,18 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import APIS from 'src/app/globals';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AddResponsService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  // lib: string, desc: string, response: string, typeid: string
+  addCategorie(data): Observable<any> {
+    return this.http.post<any>(APIS.api_postCategorie, data);
+  }
+}
